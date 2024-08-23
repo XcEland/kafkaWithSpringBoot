7c568fb1adbf723e9f7161b4f5231a5f6c8e2102
XcEland <mkoma.xyz@gmail.com>
2024-08-23 14:05:09 +0200

    streaming

---
+++ kafkaProducerRealWorld/src/main/java/com/example/kafkaProducerRealWorld/KafkaProducerRealWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaProducerRealWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerRealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerRealWorldApplication.class, args);
+	}
+
+}
---
+++ kafkaProducerRealWorld/src/test/java/com/example/kafkaProducerRealWorld/KafkaProducerRealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaProducerRealWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerRealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
